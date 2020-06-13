45e5b26b956a0b4042e30aaeab7172859c6a929d
shaulv <54745332+shaulv@users.noreply.github.com>
2020-06-14 00:27:37 +0300

    Add files via upload

---
+++ src/js/canvas.js
@@ -0,0 +1,86 @@
+import utils from './utils'
+
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const mouse = {
+  x: 10,
+  y: 10
+};
+
+const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'];
+
+// Event Listeners
+addEventListener('mousemove', (event) => {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+});
+
+addEventListener('resize', () => {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+
+  init();
+});
+
+function getDistance(x1, y1, x2, y2) {
+  let xDistance = x2 - x1;
+  let yDistance = y2 - y1;
+
+  return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+};
+
+// Objects
+class Circle {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  };
+
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath();
+  };
+
+  update() {
+    this.draw();
+  };
+}
+
+// Implementation
+let circle1, circle2;
+function init() {
+  circle1 = new Circle(500, 300, 100, 'black');
+  circle2 = new Circle(10, 10, 30, 'red');
+}
+
+// Animation Loop
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+
+  circle1.update();
+  circle2.x = mouse.x;
+  circle2.y = mouse.y;
+  circle2.update();
+
+  if (getDistance(circle1.x, circle1.y, circle2.x, circle2.y) < circle1.radius + circle2.radius) {
+    circle1.color = 'red';
+  } else {
+    circle1.color = 'black';
+  }
+
+  console.log(getDistance(circle1.x, circle1.y, circle2.x, circle2.y));
+
+}
+
+init();
+animate();
